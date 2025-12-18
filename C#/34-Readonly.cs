119a0c5af9bc45801237ed1f5ecf47d2aafa78ad
Özkan <yildizozkan6541@gmail.com>
2025-12-18 14:31:30 +0300

    Readonly

---
+++ C#/34-Readonly.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Person
+{
+    public readonly string NationalID;
+
+    public Person(string id)
+    {
+        NationalID = id; // sadece burada atanabilir
+    }
+}
+/*
+ readonly, bir değişkenin sadece nesne oluşturulurken (yani constructor içinde) atanabileceğini belirtir.
+Nesne oluşturulduktan sonra bu değişkenin değeri değiştirilemez.
+ readonly değişken isimleri genellikle Pascal Case ile yazılır: BirthDate, NationalID, SerialNumber gibi.
+ */
